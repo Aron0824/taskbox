c090ef312baaffdb6dc8759320b127e1db47c509
Aron0824 <useonjeong824@gmail.com>
2024-06-26 10:05:55 +0900

    chore: import css setting

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,31 @@
+// /** @type { import('@storybook/svelte-vite').StorybookConfig } */
+// const config = {
+//   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"],
+//   addons: [
+//     "@storybook/addon-links",
+//     "@storybook/addon-essentials",
+//     "@storybook/addon-interactions",
+//   ],
+//   staticDirs: ["../public"],
+//   framework: {
+//     name: "@storybook/svelte-vite",
+//     options: {},
+//   },
+// };
+// export default config;
+
+
 /** @type { import('@storybook/svelte-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"],
+  stories: ['../src/components/**/*.stories.js'],
+  staticDirs: ['../public'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  staticDirs: ["../public"],
   framework: {
-    name: "@storybook/svelte-vite",
+    name: '@storybook/svelte-vite',
     options: {},
   },
 };
