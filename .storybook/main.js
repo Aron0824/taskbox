9311a13c8e1b3c09411854cd1527374420d66adf
Aron0824 <useonjeong824@gmail.com>
2024-06-26 10:08:16 +0900

    chore: add a11y

--- .storybook/main.js
+++ .storybook/main.js
@@ -15,17 +15,19 @@
 // export default config;
 
 
+
 /** @type { import('@storybook/svelte-vite').StorybookConfig } */
 const config = {
   stories: ['../src/components/**/*.stories.js'],
   staticDirs: ['../public'],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    '@storybook/addon-a11y',
   ],
   framework: {
-    name: '@storybook/svelte-vite',
+    name: "@storybook/svelte-vite",
     options: {},
   },
 };
